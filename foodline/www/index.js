7316c075c3c9d73e705f3114e42f9a2541e0757d
Ahmad Ragheb <pq4@hotmail.co.uk>
2017-06-02 17:41:13 +0300

    fixing sliders and tumbsnails

--- foodline/www/index.js
+++ foodline/www/index.js
@@ -13,9 +13,9 @@ $(".nav a").on("click", function(){
 // start of slider js 
 $('#myCarousel').carousel({
   interval: 10000
-})
+ })
 
-$('.carousel .item').each(function(){
+$('#myCarousel.carousel .itemm').each(function(){
   var next = $(this).next();
   if (!next.length) {
     next = $(this).siblings(':first');
@@ -32,48 +32,33 @@ $('.carousel .item').each(function(){
 
 
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
+// function currentDiv(n) {
+//   showDivs(slideIndex = n);
+// }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+//   if (n > x.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className.replace(" w3-white", "");
+//   }
+//   x[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " w3-white";
+// }
 
 
-// moveing slider 
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 5000); 
-}
-
-// end of slider js 
+var cw = $('.thumbimg').width();
+$('.thumbimg').css({'height':(cw)+'px'});
