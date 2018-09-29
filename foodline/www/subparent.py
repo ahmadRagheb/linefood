7c3c5e5e335e220b262c1e1b9422fa53fbae49b8
ahmadRagheb <pq4@hotmail.co.uk>
2018-09-29 20:17:17 +0300

    Delete subparent.py

--- foodline/www/subparent.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-import frappe
-
-
-def get_context(context):
-    context.sliders = frappe.db.sql("select image from `tabSlider`")
-    context.marks = frappe.db.sql("select trade_mark_name,image from `tabTrade Mark`")
-    context.news = frappe.db.sql("select title,image,subject,name from `tabNews`")
-    context.products = frappe.db.sql("select product_name,image,description,choose_trade_mark from `tabProducts`")
