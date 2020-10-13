3a50be49f8543714597c8e806f6d2735eeb807c6
MaiIsmail <mai.mq.1995@gmail.com>
2020-10-13 07:12:19 +0300

    Load Data From ( Homepage ) Doctype

--- linefood/www/line_food.py
+++ linefood/www/line_food.py
@@ -7,6 +7,11 @@ def get_context(context):
   # context.marks = frappe.db.sql("select trade_mark_name,image,name from `tabTrade Mark`")
   # context.news = frappe.db.sql("select title,image,subject,name from `tabNews`")
   # context.products = frappe.db.sql("select product_name,image,description,choose_trade_mark from `tabProducts`")
-  # context.name = "Mai"
+  context.name = "Mai"
+  homepage = frappe.get_doc('Homepage')
+  context.hero = homepage.hero_section_based_on
+  context.hero_img = homepage.hero_image
+  context.title = homepage.tag_line
+  context.description = homepage.description
 
-  return {'name': 'Test Name from line_food.py'} 
+  return context
