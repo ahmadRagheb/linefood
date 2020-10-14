a2f2bab73ce204f0997b2d2348a13170270c0749
MaiIsmail <mai.mq.1995@gmail.com>
2020-10-14 07:29:11 +0300

    Fix Slideshow

--- linefood/www/line_food.py
+++ linefood/www/line_food.py
@@ -5,13 +5,19 @@ import frappe
 def get_context(context):
   # context.sliders = frappe.db.sql("select image from `tabSlider`")
   # context.marks = frappe.db.sql("select trade_mark_name,image,name from `tabTrade Mark`")
-  # context.news = frappe.db.sql("select title,image,subject,name from `tabNews`")
+  context.news = frappe.db.sql("select title,image,subject,name from `tabNews`")
   # context.products = frappe.db.sql("select product_name,image,description,choose_trade_mark from `tabProducts`")
-  context.name = "Mai"
+  context.name = "Maiiiiiiiiiiiiiiiiii"
+
   homepage = frappe.get_doc('Homepage')
   context.hero = homepage.hero_section_based_on
   context.hero_img = homepage.hero_image
   context.title = homepage.tag_line
   context.description = homepage.description
+
+  # context.test = frappe.db.sql('''select slideshow_name from `tabWebsite Slideshow`''')
+  doc = frappe.get_doc('Website Slideshow', homepage.slideshow)
+  context.slideshow = homepage.slideshow
+  context.slides = doc.slideshow_items
 
   return context
