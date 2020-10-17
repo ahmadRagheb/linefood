b9f5c15a9c4c39824766280b68b0de1dd7b11e9d
MaiIsmail <mai.mq.1995@gmail.com>
2020-10-17 03:13:10 +0300

    create line food settings doctype

---
+++ linefood/linefood/doctype/line_food_settings/line_food_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, ahmad ragheb and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Line Food Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
